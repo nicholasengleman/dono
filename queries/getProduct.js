06a516073e7c64793e50022ea8253128fb165edc
Nicholas Engleman <nicholas@englemaninvest.com>
2020-09-11 16:19:57 -0400

    created product page

--- queries/getProduct.js
+++ queries/getProduct.js
@@ -1,11 +1,14 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 export default gql`
-    query getProduct($id: uuid!) {
-        products_by_pk(id: $id) {
-            id
-            name
-            store
-        }
+  query getProduct($id: uuid!) {
+    products_by_pk(id: $id) {
+      id
+      name
+      store
+      description
+      price
+      likes
     }
+  }
 `;
